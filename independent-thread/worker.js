2f3b847739ed120cf0bc8ee34162dea328d95e3d
zjy4fun <zjy4fun@gmail.com>
2024-09-05 15:12:06 +0800

    feat: add web worker independent thread

---
+++ independent-thread/worker.js
@@ -0,0 +1,11 @@
+self.onmessage = function (event) {
+    function task() {
+        let sum = 0;
+        for (let i = 0; i < 1e9; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    const result = task();
+    self.postMessage(result);
+};
